650cd1273aa3743a26d7f42a267fb72c143944bd
Thieu Huy Hoang <20521350@gm.uit.edu.vn>
2023-12-02 23:13:27 +0700

    del

--- Lab3/Ex0/ex0.3/MyButton.js
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-class MyButton extends React.Component{
-    render(){
-        return <button>{this.props.children}</button>;
-    }
-}
-
-export default MyButton;
--- Lab3/Ex1/Ex1.0/Ex1.0.js
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import MyComponent from "./MyComponent";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(<MyComponent/>);
